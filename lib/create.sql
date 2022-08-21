4b84d67d77dc183be57bea68d1bad178681458cb
billymwangi <billy.mwangi@student.moringaschool.com>
2022-08-21 17:28:54 +0300

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears(
+    id PRIMARY KEY integer
+    name TEXT
+    age INTEGER
+    sex TEXT
+    color TEXT
+    temperament TEXT
+    alive BOOLEAN)
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", FALSE)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", TRUE)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "Nice", TRUE)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sargeant Brown", 19, "M", "Green", "Slimy", FALSE)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, "F", "dark brown", "goofy", TRUE)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "Black", "Grinchy", TRUE)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "Blue", "naive", TRUE)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 20, "M", "black", "aggressive", FALSE)
+
